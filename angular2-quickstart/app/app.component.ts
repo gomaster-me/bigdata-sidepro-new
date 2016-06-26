afafd22a05bfb6759223abe1070920a795d5b561
fqc <337940626@qq.com>
2016-06-26 22:20:00 +0800

    add quickstart ts html css file

---
+++ angular2-quickstart/app/app.component.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by fqc on 2016/6/26.
+ */
+import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App</h1>'
+})
+export class AppComponent { }
