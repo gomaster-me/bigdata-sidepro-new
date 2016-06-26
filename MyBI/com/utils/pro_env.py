431e8c2097d3c0e41918201c2807162ea212fd54
fqc <337940626@qq.com>
2016-06-26 20:47:50 +0800

    update env file format

--- MyBI/com/utils/pro_env.py
+++ MyBI/com/utils/pro_env.py
@@ -11,16 +11,8 @@ PROJECT_LIB_DIR = PROJECT_DIR + "\\lib"
 # 项目临时文件的路径
 PROJECT_TEMP_DIR = PROJECT_DIR + "\\conf"
 
-
-# export JAVA_HOME=/home/hadoop/apps/jdk
-# export HADOOP_HOME=/home/hadoop/apps/hadoop-2.6.4
-# export ZOOKEEPER_HOME=/home/hadoop/apps/zookeeper
-# export MYSQL_HOME=/usr/local/mysql
-# export HIVE_HOME=/home/hadoop/apps/hive
-# export SQOOP_HOME=/home/hadoop/apps/sqoop
-# export PATH=$JAVA_HOME/bin:$HADOOP_HOME/bin:$HADOOP_HOME/sbin:$ZOOKEEPER_HOME/bin:$MYSQL_HOME/bin:$MYSQL_HOME/lib:$HIVE_HOME/bin:$SQOOP_HOME/bin:$PATH
-# (END)
 #***********************************************
+
 # Hadoop的安装路径
 HADOOP_HOME = "/home/hadoop/apps/hadoop-2.6.4"
 # Hadoop命令的路径
@@ -36,3 +28,12 @@ SQOOP_BIN = SQOOP_HOME + "/bin"
 #***********************************************
 # Java的安装路径
 JAVA_HOME = "JAVA_HOME=/home/hadoop/apps/jdk"
+# ******************************************
+# export JAVA_HOME=/home/hadoop/apps/jdk
+# export HADOOP_HOME=/home/hadoop/apps/hadoop-2.6.4
+# export ZOOKEEPER_HOME=/home/hadoop/apps/zookeeper
+# export MYSQL_HOME=/usr/local/mysql
+# export HIVE_HOME=/home/hadoop/apps/hive
+# export SQOOP_HOME=/home/hadoop/apps/sqoop
+# export PATH=$JAVA_HOME/bin:$HADOOP_HOME/bin:$HADOOP_HOME/sbin:$ZOOKEEPER_HOME/bin:$MYSQL_HOME/bin:$MYSQL_HOME/lib:$HIVE_HOME/bin:$SQOOP_HOME/bin:$PATH
+# (END)
