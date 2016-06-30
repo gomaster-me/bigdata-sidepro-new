1002cb3c6f4a707fb8044283a693b1a2196f8ff8
fqc <337940626@qq.com>
2016-06-30 19:46:50 +0800

    merge angular2 hello world

---
+++ angular2-quickstart/app/main.js
@@ -0,0 +1,8 @@
+"use strict";
+/**
+ * Created by fqc on 2016/6/26.
+ */
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var app_component_1 = require('./app.component');
+platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent);
+//# sourceMappingURL=main.js.map
