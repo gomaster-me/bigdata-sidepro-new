1950ba161496e4d03cad3c0421897becc2d9b9e8
fqc <337940626@qq.com>
2016-07-08 13:45:05 +0800

    flowsum mr程序

---
+++ hadoop_sidepro/src/main/java/com/fan/hadoop/in_action/flowcalc/FlowCalcDriver.java
@@ -0,0 +1,47 @@
+package com.fan.hadoop.in_action.flowcalc;
+
+import com.fan.hadoop.in_action.wordcount.WCDriver;
+import com.fan.hadoop.in_action.wordcount.WCMapper;
+import com.fan.hadoop.in_action.wordcount.WCReducer;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.reduce.IntSumReducer;
+
+import java.io.IOException;
+
+/**
+ * Created by fqc on 2016/7/8.
+ */
+public class FlowCalcDriver {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+
+
+        Job job = null;
+
+        job = Job.getInstance(new Configuration());
+
+        job.setJarByClass(FlowCalcDriver.class);
+        job.setMapperClass(FlowCalcMapper.class);
+        job.setReducerClass(FlowCalcReducer.class);
+
+        /*job.setMapOutputKeyClass(Text.class); 这两段多余
+        job.setMapOutputValueClass(IntWritable.class);*/
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(FlowBean.class);
+
+        FileInputFormat.setInputPaths(job, "c:/flow/");
+        FileOutputFormat.setOutputPath(job, new Path("c:/flow/flowsum"));
+       /* FileInputFormat.setInputPaths(job, new Path(args[0]));//addInputPaths(job,string)，但两者都可以指定多个输入路径
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+*/
+        boolean res = job.waitForCompletion(true);
+        //int a = 1/0; 为了测试exit 1
+        System.exit(res ? 0 : 1);
+    }
+
+}
