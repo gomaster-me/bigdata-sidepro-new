1567e16c42960ae413efe53a087cabfd775e323a
fqc <337940626@qq.com>
2016-07-09 18:28:55 +0800

    add comment region

--- spring-boot-sidepro/src/main/java/com/fqc/springboot/model/customer.java
+++ spring-boot-sidepro/src/main/java/com/fqc/springboot/model/customer.java
@@ -18,7 +18,33 @@ public class Customer {
         this.name = name;
     }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+
+    //region comment region
     /**
      *  /*
      *  only id setter and getter
@@ -49,31 +75,6 @@ public class Customer {
     }
     ]
      */
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
+//endregion
 
 }
