0c8a6514ec65ffbc67e7f1f250a67894c48d9eba
fqc <337940626@qq.com>
2016-07-09 21:19:53 +0800

    文本类型向后台传递参数，需要@RequestBody接收

--- spring-boot-sidepro/src/main/java/com/fqc/springboot/RestFulTest.java
+++ spring-boot-sidepro/src/main/java/com/fqc/springboot/RestFulTest.java
@@ -3,17 +3,29 @@ package com.fqc.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/rest-api")
 @EnableAutoConfiguration
 public class RestFulTest {
 
-    @RequestMapping(value={"/index",""})
+    @RequestMapping(value = {"/index", ""})
     public String home() {
         return "hello spring boot";
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    //http://localhost:8080/rest-api?name=kobe
+    public String greetings_get(String name) {
+        return "hello," + name;
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    //postman中直接传递文本 需要@RequestBody接收。
+    //当然form data 直接传name value 都可以传递参数，且不需要@RequestBody
+    public String greetings_post(@RequestBody String name) {
+        return String.format("hello,%s", name);
     }
 
     public static void main(String[] args) {
