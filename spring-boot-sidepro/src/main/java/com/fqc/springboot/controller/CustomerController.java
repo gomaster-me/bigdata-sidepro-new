2d541f1fb1442fe03ecb5dede0b75e6bdfbc61ad
fqc <337940626@qq.com>
2016-07-09 23:47:06 +0800

    添加bean 返回消息 注意foreach遍历不能修改集合

--- spring-boot-sidepro/src/main/java/com/fqc/springboot/controller/CustomerController.java
+++ spring-boot-sidepro/src/main/java/com/fqc/springboot/controller/CustomerController.java
@@ -26,7 +26,7 @@ public class CustomerController {
         customerList.add(new Customer(3L, "jordan jordan"));
     }
 
-    @RequestMapping(value = {"/list",""}, method = RequestMethod.GET)
+    @RequestMapping(value = {"/list", ""}, method = RequestMethod.GET)
     public List<Customer> list() {
         return customerList;
     }
@@ -46,6 +46,42 @@ public class CustomerController {
         }
 
         return customer;
+    }
+
+    @RequestMapping(value = "/add/{name}/{id}",method = RequestMethod.GET) //为了模拟，id这里手动传一下
+    public String add(@PathVariable String name, @PathVariable Integer id) {
+        Customer customer = null;
+        String message = "";
+
+        //customerList.contains() ,可以重写id compareto方法，认为是对象相等，这里就先不做了。
+         /*for (Customer c : customerList) {
+             if (c.getId().equals(Long.parseLong(id.toString()))) {
+                 //v1注意使用equals比较对象，否则即使相等也会判断不等走到下一步修改对象了..很严重的问题
+                 //但是id传进来是固定的，遍历的对象的id是不固定的.逻辑有问题
+                 message = "已经存在";
+             } else {
+                 customer = new Customer();
+                 customer.setId(Long.parseLong(id.toString()));
+                 customerList.add(customer);
+                 message = "增加ok";
+                 //return message;; //v0注意这里一定要return或break，绝不允许再继续循环下去。
+                 break;
+             }
+        }*/
+
+        for (Customer c : customerList) {
+            if (c.getId().equals(Long.parseLong(id.toString()))) {
+                message = "已经存在";
+                return message;
+            }
+        }
+        customer = new Customer();
+        customer.setId(Long.parseLong(id.toString()));
+        customerList.add(customer);
+        message = "增加ok";
+
+        return message;
+
     }
 
     public static void main(String[] args) {
