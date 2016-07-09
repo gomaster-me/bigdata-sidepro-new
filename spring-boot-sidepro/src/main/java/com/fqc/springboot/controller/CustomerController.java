efb58b26b3310f054362e527e6a3e5c2dea0242a
fqc <337940626@qq.com>
2016-07-09 22:30:30 +0800

    修改list路由为{/list，''}，增加根据id查询bean实体 返回json

--- spring-boot-sidepro/src/main/java/com/fqc/springboot/controller/CustomerController.java
+++ spring-boot-sidepro/src/main/java/com/fqc/springboot/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.fqc.springboot.controller;
 import com.fqc.springboot.model.Customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,9 +26,26 @@ public class CustomerController {
         customerList.add(new Customer(3L, "jordan jordan"));
     }
 
-    @RequestMapping(value = "list",method = RequestMethod.GET)
+    @RequestMapping(value = {"/list",""}, method = RequestMethod.GET)
     public List<Customer> list() {
         return customerList;
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Customer getCustomerById(@PathVariable Integer id) {
+        Customer customer = null;
+        for (Customer c : customerList) {
+            if (c.getId() == Long.parseLong(id.toString())) {
+                customer = c;
+            }
+        }
+        if (customer == null) {
+            //以后可以对象统一消息，封装信息
+            //return "not found"; 或者 throw new Exception
+            return null;//暂时处理
+        }
+
+        return customer;
     }
 
     public static void main(String[] args) {
