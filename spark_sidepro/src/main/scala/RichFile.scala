01f3e5d3483b70efa0ab52f27a089564ba23d31e
fqc <337940626@qq.com>
2016-07-24 21:34:34 +0800

    定义泛型方法

--- spark_sidepro/src/main/scala/RichFile.scala
+++ spark_sidepro/src/main/scala/RichFile.scala
@@ -1,5 +1,6 @@
 import java.io.File
 
+import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
 
 /**
@@ -20,9 +21,21 @@ object ImplicitFunction extends App {
 
   val a: Int = 3.6
 
-  implicit def file2RichFile(file: File) = new RichFile(file)
+  implicit def file2RichFile(file: File) = new RichFile(file) //定义函数将要增强的类作为参数传入。
+  // 当遇到调用read方法但无read方法的时候会启动隐式转换。将File转为RichFile
 
   //RichFile 快速增强Java.io.File类
   println(new File("C:\\Users\\fqc\\IdeaProjects\\bigdata\\spark_sidepro\\src\\main\\scala\\SparkWc.scala").read)
 
+
+  //  implicit def double22Int(d: Double) = d.toInt //会有二义性，则不会发生隐式转换   
+
+  def f(x: Int) = println(x)
+
+  f(3.4)
+
+  1 to 10
 }
+//val arr = ArrayBuffer(1,23)
+//arr.sortBy()
+//arr.sortWith()
