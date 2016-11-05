434b78db4184d6b5fae50c6a0552ff5f4925f70d
fqc <337940626@qq.com>
2016-11-05 11:19:43 +0800

    @PathVariable用法 path/{param}    e.g.(baidu.com/user/{id})

--- spring-boot-sidepro/src/main/java/com/fqc/springboot/controller/IndexController.java
+++ spring-boot-sidepro/src/main/java/com/fqc/springboot/controller/IndexController.java
@@ -1,8 +1,10 @@
 package com.fqc.springboot.controller;
 
+import com.fqc.springboot.model.Customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +27,7 @@ public class IndexController {
         return "hello world test";
     }
 
-    @RequestMapping(value = "get")
+    @RequestMapping("get")
     public Map<String, String> get(@RequestParam String getMap) {
 
         Map<String, String> map = new HashMap<>();
@@ -34,6 +36,12 @@ public class IndexController {
         return map;
     }
 
+    @RequestMapping("getCustomer/{id}")
+    public Customer get(@PathVariable Integer id) {
+        System.out.println(id);
+        Customer customer = new Customer(1l, "jordan");
+        return customer;
+    }
 
 
     //@EnableAutoConfiguration 该注解在本类单独启动时需要配置，否则将会出错
