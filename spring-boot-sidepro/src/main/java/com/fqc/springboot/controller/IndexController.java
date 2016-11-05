3b0a4158b3d853a30c6fc18936d7f08e9e724485
fqc <337940626@qq.com>
2016-11-05 11:06:54 +0800

    @RequestParam的用法 e.g.( baidu.com/index/get?name)

--- spring-boot-sidepro/src/main/java/com/fqc/springboot/controller/IndexController.java
+++ spring-boot-sidepro/src/main/java/com/fqc/springboot/controller/IndexController.java
@@ -2,8 +2,13 @@ package com.fqc.springboot.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by fqc on 11/4/16.
@@ -12,12 +17,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/index")
 @EnableAutoConfiguration//该注解在本类单独启动时需要配置，否则将会出错
+//@SpringBootApplication // 该注解包含 @EnableAutoConfiguration
 public class IndexController {
 
     @RequestMapping
     public String index() {
         return "hello world test";
     }
+
+    @RequestMapping(value = "get")
+    public Map<String, String> get(@RequestParam String getMap) {
+
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "kobe");
+
+        return map;
+    }
+
+
 
     //@EnableAutoConfiguration 该注解在本类单独启动时需要配置，否则将会出错
     public static void main(String[] args) {
