2cfadc846569b9759a2b08ffecd6230c0c42753e
fqc <337940626@qq.com>
2016-11-08 07:42:41 +0800

    @PathVariable用法 path/{param}    e.g.(baidu.com/user/{id})

--- spring-boot-sidepro/src/main/java/com/fqc/springboot/controller/IndexController.java
+++ spring-boot-sidepro/src/main/java/com/fqc/springboot/controller/IndexController.java
@@ -3,7 +3,6 @@ package com.fqc.springboot.controller;
 import com.fqc.springboot.model.Customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
