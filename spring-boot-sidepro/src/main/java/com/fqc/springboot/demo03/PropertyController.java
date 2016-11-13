0dd780293ecec5b60d5131c4cf1e0aaad30e4a65
fqc <337940626@qq.com>
2016-11-14 00:05:02 +0800

    时间格式化 jackson-format

--- spring-boot-sidepro/src/main/java/com/fqc/springboot/demo03/PropertyController.java
+++ spring-boot-sidepro/src/main/java/com/fqc/springboot/demo03/PropertyController.java
@@ -1,5 +1,7 @@
 package com.fqc.springboot.demo03;
 
+import org.joda.time.DateTime;
+import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,14 +31,16 @@ public class PropertyController {
     private String randomInt;
     @Value("${app.desc}")
     private String desc;
+
     @RequestMapping("find")
-    public Map<String, String> find() {
-        Map<String, String> map = new HashMap<>();
+    public Map<String, Object> find() {
+        Map<String, Object> map = new HashMap<>();
         map.put("appName", appName);
         map.put("appCode", appCode);
         map.put("secret", random);
         map.put("number", randomInt);
         map.put("app.desc", desc);
+        map.put("date", new DateTime().toString());
         return map;
     }
 }
