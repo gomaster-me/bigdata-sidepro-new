9fb1777362d3da892085e05fad0330bce03446d1
fqc <337940626@qq.com>
2016-11-15 10:15:51 +0800

    eureka.client.serviceUrl.defaultZone

--- spring-cloud-sidepro/cloud-config-server/src/main/java/cloud/config/server/ConfigServerApplication.java
+++ spring-cloud-sidepro/cloud-config-server/src/main/java/cloud/config/server/ConfigServerApplication.java
@@ -1,13 +1,22 @@
 package cloud.config.server;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@EnableAutoConfiguration
+@EnableDiscoveryClient
 
 @SpringBootApplication
 @EnableConfigServer//读取远程配置文件，转换为rest接口服务
 public class ConfigServerApplication {
     public static void main(String[] args) {
-      SpringApplication.run(ConfigServerApplication.class, args);
+        SpringApplication.run(ConfigServerApplication.class, args);
     }
 }
+
