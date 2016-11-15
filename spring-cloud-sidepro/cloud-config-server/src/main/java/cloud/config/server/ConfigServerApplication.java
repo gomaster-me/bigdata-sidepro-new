0c8a3005b740a332563ff7eea981d9a6230f3524
fqc <337940626@qq.com>
2016-11-15 10:02:07 +0800

    调整启动项

--- spring-cloud-sidepro/cloud-config-server/src/main/java/cloud/config/server/ConfigServerApplication.java
+++ spring-cloud-sidepro/cloud-config-server/src/main/java/cloud/config/server/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.fqc.bigdata.cloud.config;
+package cloud.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +7,6 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer//读取远程配置文件，转换为rest接口服务
 public class ConfigServerApplication {
-
     public static void main(String[] args) {
       SpringApplication.run(ConfigServerApplication.class, args);
     }
