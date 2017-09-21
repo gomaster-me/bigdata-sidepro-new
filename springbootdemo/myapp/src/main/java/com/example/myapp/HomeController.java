fca2de16f30831dbd65734a19498207f4ec290c1
fqc <337940626@qq.com>
2017-09-21 13:43:58 +0800

    springboot dockerfile

--- springbootdemo/myapp/src/main/java/com/example/myapp/HomeController.java
+++ springbootdemo/myapp/src/main/java/com/example/myapp/HomeController.java
@@ -1,20 +1,32 @@
 package com.example.myapp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping; 
-import org.springframework.web.bind.annotation.RestController; 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-class HomeController {
+public class HomeController {
 
-	@RequestMapping("/")
-	public String greetings(){
-		return "hello SpringBootApplication";
-	}
+    private final Logger logger = LoggerFactory.getLogger(HomeController.class);
+    @RequestMapping("/")
+    public String greetings() {
+        logger.info("/ 根路径访问");
+        return "hello SpringBootApplication";
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(HomeController.class,args);
-	}	
+    @RequestMapping(value = "/test")
+    public String testLogLevel() {
+        logger.debug("Logger Level ：DEBUG");
+        logger.info("Logger Level ：INFO");
+        logger.error("Logger Level ：ERROR");
+        return "";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeController.class, args);
+    }
 }
